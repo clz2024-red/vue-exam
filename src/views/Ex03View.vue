53a25abf871696d2cb16cbc2e0df8c6b70b32192
clz2024.red <clz2024.red@gmail.com>
2024-03-21 15:11:32 +0900

    2024-03-21

---
+++ src/views/Ex03View.vue
@@ -0,0 +1,46 @@
+<template>
+    <h1>{{ title }}</h1>
+    <h2>{{ subTitle }}</h2>
+
+    <p>{{ counter }}</p>
+
+    <button v-on:click="minus" type="button">빼기(-)</button>
+    <button v-on:click="plus" type="button">더하기(+)</button> 
+    
+
+    <br>
+    <br>
+
+    <p>{{ message }}</p>
+    <button v-on:click="setMessage" type="button">메세지출력</button>
+
+</template>
+<script>
+
+export default {
+    name: "Ex03View",
+    components: {},
+    data() {
+        return {
+            title:"Vue공부",
+            subTitle: "클릭이벤트+메소드",
+            counter: 0,
+            message: ""
+        };
+    },
+    methods: {
+        //plus: function plus(){};
+        plus(){
+            this.counter++;
+        },
+        minus(){
+            this.counter--
+        },
+        setMessage(){
+            console.log("클릭");
+            this.message = "늦으면 연락하겠.....아니 늦지 않겠습니다.";
+        }
+    }
+};
+</script>
+<style></style>
---
+++ src/views/Ex08View.vue
@@ -0,0 +1,54 @@
+<template>
+    <h1>{{ title }}</h1>
+    <h2>{{ subTitle }}</h2>
+
+    <table border="1">
+        <thead>
+            <tr>
+                <th>번호</th>
+                <th>이름</th>
+                <th>핸드폰</th>
+                <th>회사</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-bind:key="personVo.no"  v-for="(personVo) in pList" >
+                <td></td>
+                <td>{{ personVo.name }}</td>
+                <td></td>
+                <td></td>
+            </tr>
+        </tbody>
+    </table >
+
+    <br>
+    <button v-on:click="getList" type="button">데이터불러오기</button>
+</template>
+<script>
+
+export default {
+    name: "Ex08View",
+    components: {},
+    data() {
+        return {
+            title: "vue공부",
+            subTitle: "반복문",
+            pList: []
+        };
+    },
+    methods: {
+        getList(){
+            console.log("데이터 수신중......");
+            let personList = [
+                {no: 1, name: "정우성", hp: "010-1111-1111", company: "02-1111-1111"},
+                {no: 2, name: "이효리", hp: "010-2222-2222", company: "02-2222-2222"},
+                {no: 3, name: "유재석", hp: "010-3333-3333", company: "02-3333-3333"}
+            ]
+            this.pList = personList;
+         
+
+        }
+    },
+};
+</script>
+<style></style>
